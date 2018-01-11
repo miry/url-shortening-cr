aa19b9643647843770f8921110b4a0999482678a
Michael Nikitochkin <miry.sof@gmail.com>
2018-01-11 21:27:20 +0100

    Created UrlService.

--- src/server.cr
+++ src/server.cr
@@ -1,16 +1,33 @@
 require "./version"
+require "./url_service"
 require "kemal"
+
+basename = ENV["BASENAME"]? || "http://localhost:3000"
+redis_url = ENV["REDIS_URL"]? || "redis://localhost:6379/1"
+
+UrlService.configure(redis_url, basename)
 
 get "/version" do
   Server::VERSION
 end
 
-post "/url" do
-  "http://localhost:3000/asdf"
+post "/url" do |env|
+  original_url = env.params.body["url"].as(String)
+  unless original_url
+    env.response.status_code = 422
+    %({"error": "Missing url parameter"})
+  end
+
+  service = UrlService.new(original_url)
+  short_url = service.short_url
+
+  env.response.status_code = 201
+  short_url
 end
 
 get "/:short_url" do |env|
-  env.redirect "https://google.com"
+  short_url = env.params.url["short_url"]
+  env.redirect "https://www.google.de/search?&q=" + short_url
 end
 
 Kemal.run
---
+++ src/url_service.cr
@@ -0,0 +1,40 @@
+require "redis"
+require "random/secure"
+
+class UrlService
+  def initialize(@original_url : String)
+  end
+
+  def self.configure(redis_url : String, basename : String)
+    @@redis = Redis.new(url: redis_url)
+    @@basename = basename
+  end
+
+  def self.redis
+    @@redis || Redis.new
+  end
+
+  def short_url
+    path = endpoint || create
+    "#{@@basename}/#{path}"
+  end
+
+  def endpoint
+    self.class.redis.get(@original_url)
+  end
+
+  def create
+    short_url = ""
+    existing = 1
+    redis = self.class.redis
+    while existing == 1
+      short_url = Random::Secure.urlsafe_base64(6)
+      existing = redis.exists(short_url)
+    end
+
+    redis.set(@original_url, short_url)
+    redis.set(short_url, @original_url)
+
+    return short_url
+  end
+end
